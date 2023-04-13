e3f3b6784ed7ab1b37f95cccbc4f28e98dab476e
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2023-04-13 07:49:34 +0530

    added system color preference

--- src/App.js
+++ src/App.js
@@ -20,13 +20,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import darkScrollbar from '@mui/material/darkScrollbar';
 import { useEffect, useState } from "react";
 import { Box, CircularProgress } from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 //const BASE_URL = 'http://localhost:3000';
 const BASE_URL = 'https://opportunity-portal.edciitd.com';
 
-const timer=1500;
+const timer = 1500;
 
 export default function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const [mode, setMode] = useState('light');
     const [startUpDetails, setStartUpDetails] = useState(null);
     const [studentDetails, setStudentDetails] = useState(null);
@@ -119,6 +121,12 @@ export default function App() {
         if (localStorage.getItem('colorMode') === 'dark') {
             setMode('dark');
         }
+        else if(localStorage.getItem('colorMode') === 'light'){
+            setMode('light');
+        }
+        else{
+            setMode(prefersDarkMode?'dark':'light');
+        }
     }, [])
 
     return (
@@ -129,25 +137,25 @@ export default function App() {
                     <BrowserRouter>
                         <Routes>
                             <Route path="/" element={<JobPortalIndex mode={mode} setMode={setMode} startUpDetails={startUpDetails} studentDetails={studentDetails} />}>
-                                <Route path="/" element={<StudentOrStartUp BASE_URL={BASE_URL} timer={timer}/>} />
+                                <Route path="/" element={<StudentOrStartUp BASE_URL={BASE_URL} timer={timer} />} />
                                 <Route path="details" element={<JobDetails BASE_URL={BASE_URL} startUpDetails={null} />} />
-                                <Route path="signIn" element={<SignIn BASE_URL={BASE_URL}/>} timer={timer}/>
-                                <Route path="signUp" element={<SignUp BASE_URL={BASE_URL}/>} timer={timer}/>
-                                <Route path="otpVerify" element={<OTPVerify BASE_URL={BASE_URL} setStartUpDetails={setStartUpDetails} setStudentDetails={setStudentDetails} timer={timer}/>} />
+                                <Route path="signIn" element={<SignIn BASE_URL={BASE_URL} />} timer={timer} />
+                                <Route path="signUp" element={<SignUp BASE_URL={BASE_URL} />} timer={timer} />
+                                <Route path="otpVerify" element={<OTPVerify BASE_URL={BASE_URL} setStartUpDetails={setStartUpDetails} setStudentDetails={setStudentDetails} timer={timer} />} />
                             </Route>
 
                             <Route path="student" element={<StudentIndex mode={mode} setMode={setMode} studentDetails={studentDetails} setStudentDetails={setStudentDetails} />}>
-                                <Route path="internship" element={<StudentInternship BASE_URL={BASE_URL} studentDetails={studentDetails} timer={timer}/>} />
-                                <Route path="account" element={<StudentAccount BASE_URL={BASE_URL} studentDetails={studentDetails} setStudentDetails={setStudentDetails} timer={timer}/>} />
+                                <Route path="internship" element={<StudentInternship BASE_URL={BASE_URL} studentDetails={studentDetails} timer={timer} />} />
+                                <Route path="account" element={<StudentAccount BASE_URL={BASE_URL} studentDetails={studentDetails} setStudentDetails={setStudentDetails} timer={timer} />} />
                                 <Route path="details" element={<JobDetails BASE_URL={BASE_URL} startUpDetails={null} />} />
-                                <Route path="apply" element={<StudentApply BASE_URL={BASE_URL} studentDetails={studentDetails} timer={timer}/>} />
+                                <Route path="apply" element={<StudentApply BASE_URL={BASE_URL} studentDetails={studentDetails} timer={timer} />} />
                             </Route>
 
                             <Route path="startUp" element={<StartUpIndex mode={mode} setMode={setMode} startUpDetails={startUpDetails} setStartUpDetails={setStartUpDetails} />}>
                                 <Route path="internship" element={<StartUpInternship BASE_URL={BASE_URL} startUpDetails={startUpDetails} />} />
-                                <Route path="account" element={<StartUpAccount BASE_URL={BASE_URL} startUpDetails={startUpDetails} setStartUpDetails={setStartUpDetails} timer={timer}/>} />
-                                <Route path="addNew" element={<StartUpAddNew BASE_URL={BASE_URL} timer={timer}/>} />
-                                <Route path="studentsApplied" element={<StudentsApplied BASE_URL={BASE_URL} timer={timer}/>} />
+                                <Route path="account" element={<StartUpAccount BASE_URL={BASE_URL} startUpDetails={startUpDetails} setStartUpDetails={setStartUpDetails} timer={timer} />} />
+                                <Route path="addNew" element={<StartUpAddNew BASE_URL={BASE_URL} timer={timer} />} />
+                                <Route path="studentsApplied" element={<StudentsApplied BASE_URL={BASE_URL} timer={timer} />} />
                                 <Route path="details" element={<JobDetails BASE_URL={BASE_URL} startUpDetails={startUpDetails} />} />
                             </Route>
 
